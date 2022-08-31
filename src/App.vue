11189bf84de7aebaf128104ac1632e95b7c851e5
shervan <shervangh@gmail.com>
2022-09-01 01:18:01 +0430

    set router links for comps

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Pop></Pop>
+  <router-link to="/"></router-link>
+  <router-link :to="{name : 'ChatsView'}"></router-link>
+  <router-link :to="{name : 'FriendsView'}"></router-link>
+  <router-link :to="{name : 'FavoritesVeiw'}"></router-link>
+  <router-view/>
+</template>
+
+
+   <script>
+    import Pop from './components/Pop'
+
+    export default {
+     data(){
+       return{
+       }
+     },
+      components:{
+       Pop
+      }
+   }
+   </script>
+
+
